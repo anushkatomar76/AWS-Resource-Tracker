dfbcb2d3b75d6710abb6e25d2a0af373ca61a8b7
Ubuntu <ubuntu@ip-172-31-94-101.ec2.internal>
2024-09-06 18:09:21 +0000

    usage of aws

---
+++ aws_resourcetracker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+################################
+
+# Author: Anushka
+#
+# Date: 6th-sep
+#
+# Version: v1
+#
+# This script will report the AWS resource usage
+#
+################################
+
+
+set -x
+
+#AWS S3
+#AWS EC2
+#AWS Lambda
+#AWS IAM Users
+
+#List S3 buckets
+echo "Print list of s3 buckets"
+aws s3 ls
+
+#List EC2 instance
+echo "Print list of ec2 instances"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+#List aws lambda
+echo "Print list of lambda functions"
+aws lambda list-functions
+
+#List IAM Users
+echo "Print list of IAM user"
+aws iam list-users
